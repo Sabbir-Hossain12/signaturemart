236d7e7b8becd8a181c0a419abac4c0b32247c62
Sabbir-Hossain12 <h.sabbir36@yahoo.com>
2025-07-30 18:26:00 +0600

    Admin: admin module v1

---
+++ resources/views/admin/include/sidebar.blade.php
@@ -0,0 +1,70 @@
+<!-- partial:partials/_sidebar.html -->
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <div class="d-flex sidebar-profile">
+                <div class="sidebar-profile-image">
+                    <img src="{{ asset('public/admin') }}/images/faces/face29.png" alt="image">
+                    <span class="sidebar-status-indicator"></span>
+                </div>
+                <div class="sidebar-profile-name">
+                    <p class="sidebar-name">
+                        Kenneth Osborne
+                    </p>
+                    <p class="sidebar-designation">
+                        Welcome
+                    </p>
+                </div>
+            </div>
+            <div class="nav-search">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Type to search..." aria-label="search" aria-describedby="search">
+                    <div class="input-group-append">
+                  <span class="input-group-text" id="search">
+                    <i class="typcn typcn-zoom"></i>
+                  </span>
+                    </div>
+                </div>
+            </div>
+            <p class="sidebar-menu-title">Dash menu</p>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('admin.dashboard') }}">
+                <i class="typcn typcn-device-desktop menu-icon"></i>
+                <span class="menu-title">Dashboard </span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="typcn typcn-briefcase menu-icon"></i>
+                <span class="menu-title">Admins</span>
+                <i class="typcn typcn-chevron-right menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="{{ route('admin.admin.index') }}">Admins</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Roles</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Permissions</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+                <i class="typcn typcn-film menu-icon"></i>
+                <span class="menu-title">Form elements</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="form-elements">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
+                </ul>
+            </div>
+        </li>
+
+
+
+
+    </ul>
+
+</nav>
+<!-- partial -->
